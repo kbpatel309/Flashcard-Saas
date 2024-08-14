abbf9e1bfacc4c4719a492e959a28521bb455d98
Kishan Patel <kbpatel309@gmail.com>
2024-08-13 23:36:09 -0500

    Basic Nextjs setup

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCdDafBnNAHQqBy5g4Ikv2i-1Af1d4CtkQ",
+  authDomain: "flashcard-saas-2111f.firebaseapp.com",
+  projectId: "flashcard-saas-2111f",
+  storageBucket: "flashcard-saas-2111f.appspot.com",
+  messagingSenderId: "652826455432",
+  appId: "1:652826455432:web:308789003aacb2db6ade3d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
